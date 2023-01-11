8b449ca36bd4f01eddf2c43e392dde40876da944
Philipp Mitrofanov <press.fm@mail.ru>
2023-01-11 18:45:02 +0300

    Переименовал упражнение 03

---
+++ Example_03/Program.cs
@@ -0,0 +1,52 @@
+﻿void InputMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        matrix [i,j] = new Random (). Next (0, 11);
+    }
+}
+
+void PrintMatrix (int [,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void ReleaseMatrix (int [,] FirstMatrix, int [,] SecondMatrix)
+{
+    int [,] ResultMatrix = new int [FirstMatrix.GetLength(0), FirstMatrix.GetLength(1)];
+    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+        {
+            ResultMatrix [i, j] = FirstMatrix [i, j] * SecondMatrix [i, j];
+            Console.Write($"{ResultMatrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матриц: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] FirstMatrix = new int[size[0], size[1]];
+int[,] SecondMatrix = new int[size[0], size[1]];
+InputMatrix(FirstMatrix);
+InputMatrix(SecondMatrix);
+Console.WriteLine("Начальный массив 1:");
+PrintMatrix(FirstMatrix);
+Console.WriteLine("Начальный массив 2:");
+PrintMatrix(SecondMatrix);
+Console.WriteLine("Итоговый массив");
+ReleaseMatrix (FirstMatrix, SecondMatrix);
